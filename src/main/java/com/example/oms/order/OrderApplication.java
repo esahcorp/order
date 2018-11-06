464d9041802edb38bcdddf030dfbbf43b0758b84
Cambrian <hero_0@outlook.com>
2018-11-06 14:53:54 +0800

    build(*): init commit

---
+++ src/main/java/com/example/oms/order/OrderApplication.java
@@ -0,0 +1,15 @@
+package com.example.oms.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cambrian
+ */
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
